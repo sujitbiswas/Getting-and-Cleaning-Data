4239436e728aaefb465e4c32e51d571d889f2fd7
sujitbiswas <sujit.biswas@gmail.com>
2014-07-21 21:06:32 -0700

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -3,8 +3,8 @@
 # Read features and cleanup/normalize some characters
 features = read.csv("features.txt", sep="", header=FALSE)
 features[,2] = gsub('[()]', '', features[,2])
-features[,2] = gsub('mean', 'Mean', features[,2])
-features[,2] = gsub('std', 'StandardDev', features[,2])
+features[,2] = gsub('-mean', '-Mean', features[,2])
+features[,2] = gsub('-std', '-StandardDev', features[,2])
 
 #read training data
 train = read.csv("train/X_train.txt", sep="", header=FALSE)
@@ -20,7 +20,7 @@ test[,563] = read.csv("test/subject_test.txt", sep="", header=FALSE)
 allData = rbind(train, test)
 
 # Get only the data on mean and std. dev.
-relevantCols <- grep(".*Mean.*|.*StandardDev.*", features[,2])
+relevantCols <- grep(".*-Mean.*|.*-StandardDev.*", features[,2])
 # get only relevant features
 features <- features[relevantCols,]
 # add the last two columns - subject & activity
@@ -28,17 +28,20 @@ relevantCols <- c(relevantCols, 562, 563)
 # create relevant data set
 relevantData <- allData[,relevantCols]
 # Add the column names (features) to relevantData
-colnames(relevantData) <- c(features$V2, "Activity", "Subject")
+colnames(relevantData) <- c(features$V2, "activity", "subject")
 
 #create tidy data
-tidy = aggregate(relevantData, by=list(Activity = relevantData$Activity, Subject=relevantData$Subject), mean)
-# Remove the subject and activity column as they are duplicate
-tidy[,90] = NULL
-tidy[,89] = NULL
+tidy = aggregate(relevantData, by=list(relevantData$activity, relevantData$subject), mean)
+# Remove the two groups created
+tidy[,"Group.1"] = NULL
+tidy[,"Group.2"] = NULL
+
+#finally cleanup the '-' in the column names
+colnames(tidy) <- gsub('-', '', colnames(tidy))
 
 #read activity labels and use them to set in the tidy data
 activityLabels = read.csv("activity_labels.txt", sep="", header=FALSE)
-tidy$Activity = factor(tidy$Activity, levels=c(1,2,3,4,5,6),labels=activityLabels$V2)
+tidy$activity = factor(tidy$activity, levels=c(1,2,3,4,5,6),labels=activityLabels$V2)
 
 #wrtie tidy data
 write.table(tidy, "tidy.txt", sep="\t")
